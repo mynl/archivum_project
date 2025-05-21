18a21f78fb0c54a66ce6996b08b9ed5d7be10181
Stephen Mildenhall <steve@mynl.com>
2025-05-21 10:10:08 +0100

    porting to mendeley progress

--- port_mendeley/README.md
+++ port_mendeley/README.md
@@ -1,21 +1,3 @@
 # Port Mendeley
 
 --> see Joplin
-
-One-time steps to port existing Mendeley libary into archivum. No attempt to be generic!
-
-Last added file is Mark Browne NAAJ 2025.
-
-## Steps
-
-1. Bibtex to dataframe
-2. Normalize author names
-    * De-TeX
-    * Initials
-3. Normalize journals: de-TeX
-4. Normalize publishers: de-TeX
-5. Validate linked files / fuzzy look up
-6. Normalize tags
-6. Update dataframe
-7. Recreate Bib file with selected fields
-8. Create qmd file tester file with all tags
--- temp/scratch.py
+++ temp/scratch.py
@@ -1,5 +1,6 @@
 """Script experiments."""
 
+from collections import namedtuple
 from pathlib import Path
 import re
 
@@ -16,6 +17,35 @@ class Bib2df():
     _r_initials2 = re.compile(r'([A-Z])(?:\.| )?([A-Z])(\.|$)')   # for AH -> A. H.
     _r_initials3 = re.compile(r' ([A-Z])$')            # for A  -> A.
 
+    _char_unicode_dict = {
+        '“': '"',    # left double quote
+        '”': '"',    # right double quote
+        '„': '"',    # low double quote
+        '«': '"',    # double angle quote
+        '»': '"',
+        '′': "'",
+        '‘': "'",    # left single quote
+        '’': "'",    # right single quote
+        '‚': "'",    # low single quote
+        '′': "'",    # prime
+        '‵': "'",    # reversed prime
+        '‹': "'",    # single angle quote
+        '›': "'",
+
+        '\u00a0': ' ',    # non-breaking space
+        '\u200b': '',     # zero-width space
+        '\ufeff': '',     # BOM
+    }
+
+    _char_map = str.maketrans(_char_unicode_dict)
+
+    # for regexp based fixes
+    _re_subs = {
+        '–': '--',    # en dash → hyphen
+        '—': '---',    # em dash → hyphen
+    }
+    _re_subs_compiled = re.compile('|'.join(map(re.escape, _re_subs)))
+
 
     def __init__(self, p, fillna=True):
         """
@@ -23,7 +53,9 @@ class Bib2df():
 
         Use fillna=False to use contents.
         """
-        self.txt = p.read_text(encoding='utf-8')
+        self.txt = p.read_text(encoding='utf-8').translate(self._char_map)
+        self.txt, n = self._re_subs_compiled.subn(lambda m: self._re_subs[m.group()], self.txt)
+        print(f'uber regex sub found {n = } replacements')
         self.stxt = re.split(r'^@', self.txt, flags=re.MULTILINE)
         l = map(self.parse_line, self.stxt[1:])
         self._df = pd.DataFrame(l)
@@ -34,6 +66,9 @@ class Bib2df():
             self._df = self._df.fillna('')
         self._file_field_df = None
         self._file_errs = None
+        self.ported_df = None
+        self._author_map_df = None
+        self.all_unicode_errors = None
 
     @property
     def df(self):
@@ -173,15 +208,18 @@ class Bib2df():
         cdf = pd.DataFrame(ans, columns=['column', 'nonna', 'distinct'])
         return cdf
 
+    @property
     def author_map_df(self):
         """DataFrame of author name showing a transition to a normalized form."""
-        df = pd.DataFrame({'original': self.distinct('author')})
-        self.last_decode = []
-        df['unicoded'] = df.original.map(self.tex_to_unicode)
-        df['initials'] = df.unicoded.map(self.normalize_initials)
-        df['proposed'] = df.initials
-        print(f'Decode errors: {len(self.last_decode) = }')
-        return df
+        if self._author_map_df is None:
+            df = pd.DataFrame({'original': self.distinct('author')})
+            self.last_decode = []
+            df['unicoded'] = df.original.map(self.tex_to_unicode)
+            df['initials'] = df.unicoded.map(self.normalize_initials)
+            df['proposed'] = df.initials
+            print(f'Field: authors\nDecode errors: {len(self.last_decode) = }')
+            self._author_map_df = df
+        return self._author_map_df
 
     def journal_map_df(self):
         df = pd.DataFrame({'original': self.distinct('journal')})
@@ -199,16 +237,39 @@ class Bib2df():
         else:
             return sorted(set([i for i in self.df[c] if i != '']))
 
-    def tex_to_unicode(self, s: str) -> str:
+    _errors_mapper = {'Caicedo, Andr´es Eduardo': 'Caicedo, Andrés Eduardo',
+ 'Cerreia‐Vioglio, Simone': 'Cerreia‐Vioglio, Simone',
+ 'Cerreia–Vioglio, S.': 'Cerreia–Vioglio, S.',
+ 'Cireşan, Dan': 'Cireșan, Dan',
+ 'J.B., SEOANE-SEP´ULVEDA': 'J.B., Seoane-Sepúlveda',
+ 'JIM´ENEZ-RODR´IGUEZ, P.': 'Jiménez-Rodríguez, P.',
+ 'Joldeş, Mioara': 'Joldeș, Mioara',
+ 'Lesne, Jean‐Philippe ‐P': 'Lesne, Jean‐Philippe ‐P',
+ 'MU˜NOZ-FERN´ANDEZ, G.A.': 'Muñoz-Fernández, G.A.',
+ 'Naneş, Ana Maria': 'Naneș, Ana Maria',
+ 'Paradıs, J': 'Paradís, J',
+ "P{\\'{a}}stor, Ľ": 'Pástor, Ľ',
+ 'Uludağ, Muhammed': 'Uludağ, Muhammed',
+ 'Ulug{\\"{u}}lyaǧci, Abdurrahman': 'Ulugülyaǧci, Abdurrahman',
+ 'Zitikis, Riċardas': 'Zitikis, Riċardas',
+ 'de la Pen̄a, Victor H.': 'de la Peña, Victor H.',
+ "{L{\\'{o}}pez\xa0de\xa0Vergara}, Jorge E.": 'López\xa0de\xa0Vergara, Jorge E.'}
+
+    def tex_to_unicode(self, s_in: str) -> str:
         """Tex codes to Unicode for a string and removing braces with single character."""
+        if pd.isna(s_in):
+            return s_in
         try:
-            s = self._r_brace2.sub(r'\1', s.encode('latin1').decode('latex'))
+            s = self._r_brace2.sub(r'\1', s_in.encode('latin1').decode('latex'))
             s= self._r_brace1.sub(r'\1', s)
             if s.find(',') > 0 and s == s.upper():
                 s = s.title()
             return s
         except ValueError as e:
-            self.last_decode.append(f'tex_to_unicode DECODE ERR | {s:<25s} | {e}')
+            s = self._errors_mapper.get(s_in, s_in)
+            if s_in not in self._errors_mapper:
+                self.last_decode.append(s_in)
+            # (f'tex_to_unicode DECODE ERR | {s:<25s} | {e}')
             return s
 
     _r_filter = re.compile(r'^[A-Z]{,5}$')
@@ -226,3 +287,98 @@ class Bib2df():
         except ValueError as e:
             self.last_decode.append(f'normalize_initials INITIALS ERR: {s:<25s}\t{e}')
             return s
+
+    def author_last_multiple_firsts(self):
+        """Last names with multiple firsts, showing the parts."""
+        df = self.author_map_df
+        df[['last', 'rest']] = df['proposed'].str.split(',', n=1, expand=True)
+        df['rest'] = df['rest'].str.strip()
+
+        return (df.fillna('')
+            .groupby('last')
+            .apply(lambda x:
+                pd.Series([len(x), sorted(set(x.rest))], index=('n', 'set')),
+                include_groups=False)
+            .query('n>1'))
+
+    def author_mapper(self):
+        """dict mapper for author name."""
+        mapper =  {k: v for k, v in self.author_map_df[['original', 'proposed']].values}
+        # manual fixes
+        manual_updates = {
+            'Acemoglu, By Daron': 'Acemoglu, Daron',
+            'Candes, E': 'Candès, Emmanuel J.',
+            'Candes, E.': 'Candès, Emmanuel J.',
+            'Candes, E.J.': 'Candès, Emmanuel J.',
+            'Candes, Emmanuel': 'Candès, Emmanuel J.',
+            'Candes, Emmanuel J.': 'Candès, Emmanuel J.',
+            'Cand{\\`{e}}s, Emamnuel J': 'Candès, Emmanuel J.',
+            'Cand{\\`{e}}s, Emmanuel J': 'Candès, Emmanuel J.',
+            'Cand{\\`{e}}s, Emmanuel J.': 'Candès, Emmanuel J.'}
+        mapper.update(manual_updates)
+        return mapper
+
+    def port_mendeley_file(self):
+        """
+        Normalize each text-based field.
+
+        Runs through each task in turn, see comments.
+        """
+        dropped_fields = ['abstract', 'annote', 'issn', 'isbn', 'archivePrefix', 'arxivId', 'eprint', 'pmid',
+        'primaryClass', 'series', 'chapter', 'school',
+        'organization', 'howpublished'
+        ]
+        kept_fields = [i for i in self.df.columns if i not in dropped_fields]
+
+        self.ported_df = self.df[kept_fields].copy()
+
+        # ============================================================================================
+        # author
+        am = self.author_mapper()
+        def a_fix(v):
+            a = v.split(' and ')
+            anew = [am.get(i,i) for i in a]
+            return ' and '.join(anew)
+        self.ported_df.author = self.ported_df.author.map(a_fix)
+        print('fixed authors')
+
+        # ============================================================================================
+        # de-tex other text fields
+        self.all_unicode_errors = {}
+        for f in ['title', 'journal', 'publisher', 'institution', 'booktitle', 'address', 'editor',
+
+        ]:
+            self.last_decode = []
+            self.ported_df[f] = self.ported_df[f].map(self.tex_to_unicode)
+            if len(self.last_decode):
+                print(f'Field: {f}\n{len(self.last_decode) = }')
+                self.all_unicode_errors[f] = self.last_decode.copy()
+            print(f'Fixed {f}')
+
+        # ============================================================================================
+        # keywords
+
+        # ============================================================================================
+        # mendeley-tags
+
+        # ============================================================================================
+        # edition
+
+        # ============================================================================================
+        # files(!)
+
+        # ============================================================================================
+        # tags and duplicates
+
+        # ============================================================================================
+        # final checks and balances
+
+
+    def show_unicode_errors(self):
+        """Accumulated unicode errors."""
+        if self.all_unicode_errors is None:
+            return None
+        ans = set()
+        for k, v in self.all_unicode_errors.items():
+            ans = ans.union(set([c for l in v for c in l if len(c.encode('utf-8')) > 1]))
+        return ans
