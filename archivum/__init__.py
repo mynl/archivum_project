4421bb40ecb47c964c2f3b8f583c40301abff1d2
Stephen Mildenhall <steve@convexrisk.com>
2025-05-24 23:01:50 +0100

    attempt at platform independent directories

--- archivum/__init__.py
+++ archivum/__init__.py
@@ -1,5 +1,7 @@
 """archivum project."""
 
+from . __version__ import __version__
+import sys
 import os
 from pathlib import Path
 
@@ -9,12 +11,24 @@ __author__ = 'Stephen Mildenhall'
 __date__ = '2025-05-22'
 
 
+# def _get_local_folder():
+#     local_app_data = Path(os.environ["LOCALAPPDATA"])
+#     my_app_data = local_app_data / __appname__
+#     # print(my_app_data)
+#     assert my_app_data.exists(), 'Application database does not exist.'
+#     # my_app_data.mkdir(parents=True, exist_ok=True)
+#     return my_app_data
+
+
 def _get_local_folder():
-    local_app_data = Path(os.environ["LOCALAPPDATA"])
-    my_app_data = local_app_data / __appname__
-    # print(my_app_data)
-    assert my_app_data.exists(), 'Application database does not exist.'
-    # my_app_data.mkdir(parents=True, exist_ok=True)
+    if sys.platform == "win32":
+        base = Path(os.environ["LOCALAPPDATA"])
+    else:
+        base = Path(os.environ.get("XDG_DATA_HOME", Path.home() / ".local" / "share"))
+    my_app_data = base / __appname__
+    if not my_app_data.exists():
+        my_app_data.mkdir(parents=True, exist_ok=True)
+        # raise FileNotFoundError("Application database does not exist.")
     return my_app_data
 
 
@@ -26,10 +40,8 @@ APP_NAME = __appname__
 APP_SUFFIX = '.archivum-config'
 DEFAULT_CONFIG_FILE = BASE_DIR / f"default{APP_SUFFIX}"
 
-BIBTEX_DIR = "\\s\\telos\\biblio\\"
+# BIBTEX_DIR = "\\s\\telos\\biblio\\"
 
 
 # avoid circular import errors, import here
-from . __version__ import __version__
 from . library import Library  # noqa
-
