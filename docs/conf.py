a9f2f5d6df28d417ad8a05bb84df5159b8f527b0
Stephen Mildenhall <steve@convexrisk.com>
2025-12-01 10:09:28 +0000

    removed sphinx panels dependency

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,9 @@ import archivum
 
 # -- Project information -----------------------------------------------------
 
-project = 'archivum'
-copyright = '2025, Stephen J Mildenhall'
-author = 'Stephen J Mildenhall'
+project = "archivum"
+copyright = "2025, Stephen J Mildenhall"
+author = "Stephen J Mildenhall"
 
 # The full version, including alpha/beta/rc tags
 release = archivum.__version__
@@ -33,30 +33,30 @@ release = archivum.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.autosectionlabel',
-    'sphinx_copybutton',
-    'sphinx_toggleprompt',
-    'IPython.sphinxext.ipython_directive',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'nbsphinx',
-    'sphinx_panels',
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autosectionlabel",
+    "sphinx_copybutton",
+    "sphinx_toggleprompt",
+    "IPython.sphinxext.ipython_directive",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "nbsphinx",
+    # 'sphinx_panels',
     # 'sphinxcontrib.bibtex',
-    'sphinx_multitoc_numbering',
-    'sphinx_rtd_dark_mode'
+    "sphinx_multitoc_numbering",
+    "sphinx_rtd_dark_mode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # bibtex options
@@ -66,10 +66,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # user starts in light mode
 default_dark_mode = False
@@ -78,24 +78,24 @@ default_dark_mode = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 html_theme_options = {
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'both',
-    'style_external_links': False,
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 3,
-    'includehidden': True,
-    'titles_only': False,
+    "logo_only": False,
+    "display_version": True,
+    "prev_next_buttons_location": "both",
+    "style_external_links": False,
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 3,
+    "includehidden": True,
+    "titles_only": False,
 }
 
-html_logo = '_static/archivum-logo.png'
-html_favicon = '_static/favicon.ico'
+html_logo = "_static/archivum-logo.png"
+html_favicon = "_static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
